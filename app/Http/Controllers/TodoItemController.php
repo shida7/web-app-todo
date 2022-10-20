653a9fca14789420f4539db099ffad4b45eba46b
shida7 <kd1264392@st.kobedenshi.ac.jp>
2022-10-20 22:48:27 +0900

    更新

--- app/Http/Controllers/TodoItemController.php
+++ app/Http/Controllers/TodoItemController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Auth;
 class TodoItemController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * [GET|HEAD] Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
@@ -21,7 +21,7 @@ class TodoItemController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+     * [POST] Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
@@ -40,36 +40,74 @@ class TodoItemController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * [GET|HEAD] Display the specified resource.
      *
      * @param  \App\Models\TodoItem  $todoItem
      * @return \Illuminate\Http\Response
      */
-    public function show(TodoItem $todoItem)
+    public function show(TodoItem $todoItem, $id)
     {
-        //
+        $item = TodoItem::where('id', $id)->where('user_id', Auth::user()->id)->first();
+        if (!$item) return abort(404);
+        return $item;
     }
 
     /**
-     * Update the specified resource in storage.
+     * [PUT|PATCH] Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\TodoItem  $todoItem
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, TodoItem $todoItem)
+    public function update(Request $request, TodoItem $todoItem, $id)
     {
-        //
+        $item = TodoItem::where('id', $id)->where('user_id', Auth::user()->id)->first();
+        if (!$item) return abort(403);
+        $request->validate([
+            'item-contents' => 'required|max:100',
+        ]);
+        $item->title = $request->input('item-contents');
+        $item->save();
+        return array('success' => true);
     }
 
     /**
-     * Remove the specified resource from storage.
+     * [DELETE] Remove the specified resource from storage.
      *
      * @param  \App\Models\TodoItem  $todoItem
      * @return \Illuminate\Http\Response
      */
-    public function destroy(TodoItem $todoItem)
+    public function destroy(TodoItem $todoItem, $id)
     {
-        //
+        TodoItem::where('id', $id)->where('user_id', Auth::user()->id)->delete();
+        return array('success' => true);
+    }
+
+    /**
+     * [PATCH] Update the specified resource in storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function done(Request $request, $id)
+    {
+        $item = TodoItem::where('id', $id)->where('user_id', Auth::user()->id)->first();
+        $item->is_done = true;
+        $item->save();
+        return array('success' => true);
+    }
+
+    /**
+     * [PATCH] Update the specified resource in storage.
+     *
+     * @param int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function undone(Request $request, $id)
+    {
+        $item = TodoItem::where('id', $id)->where('user_id', Auth::user()->id)->first();
+        $item->is_done = false;
+        $item->save();
+        return array('success' => true);
     }
 }
